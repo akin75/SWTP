6105bed03ed4d85878c827b833bcdac641814545
UfukGlr <ugueler54@gmail.com>
2023-05-02 12:36:06 +0200

    unfinished new PlayerController: Aiming doesnt work. MousePosition wont be captured correctly

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,37 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-      public GameObject bulletPrefab;
-      public Transform firePoint;
-      public float fireForce = 20f;
-      
-      public float timeBetweenShots = 0.02f;
-      private float timeSinceLastShot = 0f;
+    public Rigidbody2D rb;
 
-      public float maxDeviation = 10f;
-
-      public void Update()
-      {
-            timeSinceLastShot += Time.deltaTime;
-            if (Input.GetButton("Fire1") && timeSinceLastShot >= timeBetweenShots)
-            {
-                  Fire();
-                  timeSinceLastShot = 0f;
-            }
-      }
-
-      public void Fire()
-      {
-            // Generate random deviation angle
-            float deviationAngle = UnityEngine.Random.Range(-maxDeviation, maxDeviation);
-            Quaternion deviation = Quaternion.Euler(0, 0, deviationAngle);
-            
-            // Create bullet and apply deviation to rotation
-            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation * deviation);
-            bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.up * fireForce, ForceMode2D.Impulse);
-      }
+    public void Aim(Vector3 targetPosition)
+    {
+        Vector3 direction = targetPosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
+        rb.rotation = angle;
+    }
 }
