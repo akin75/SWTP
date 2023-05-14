790df47987ab14edd23533c5f226104d0372b1d9
AndreMNI <andre.schulz@mni.thm.de>
2023-05-14 11:41:09 +0200

    health system added

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,7 +8,8 @@ public class Bullet : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Destroy(collision.gameObject);
+            collision.gameObject.GetComponent<Enemy>().TakeDamage(40);
+            // Destroy(collision.gameObject);
         }
         Destroy(gameObject);
     }
