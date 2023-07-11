3ffab5bfc62653327ab61b9e13b9831dd3c52cfd
UfukGlr <ugueler54@gmail.com>
2023-07-11 20:54:29 +0200

    Recoil on Dual Pistol now is dynamic

---
+++ Assets/PostProcessingHandling.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessingHandling : MonoBehaviour
+{
+    private Player player;
+    private Volume volume;
+    private Vignette vignette;
+    [SerializeField] private AnimationCurve vignetteIntensity;
+    public AudioSource heartbeatSlowSfx;
+    public AudioSource heartbeatFastSfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GameObject.FindGameObjectWithTag("GlobalVolume").GetComponent<Volume>();
+        volume.profile.TryGet(out vignette);
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LowHealthEffect();
+        HeartBeat();
+    }
+    
+    private void LowHealthEffect()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        float currentHealth = player.GetCurrentHealth();
+        if (vignette != null)
+        {
+            if (currentHealth <= 50)
+            { 
+                vignette.color.Override(new Color(0.3f, 0f, 0f)); // Dunkelrot mit RGB-Werten von (0.5, 0, 0)
+                float intensity = vignetteIntensity.Evaluate(100f - currentHealth);
+                vignette.intensity.Override(intensity);
+            } else
+            {
+                vignette.color.Override(Color.black);
+                vignette.intensity.Override(0.4f);
+            }
+        }
+    }
+
+    private void HeartBeat()
+    {
+        float currentHealth = player.GetCurrentHealth();
+        if (currentHealth <= 50)
+        {
+            heartbeatSlowSfx.mute = false;
+            heartbeatFastSfx.mute = true;
+        } else if (currentHealth <= 25)
+        {
+            heartbeatSlowSfx.mute = true;
+            heartbeatFastSfx.mute = false;
+        }
+        else
+        {
+            heartbeatSlowSfx.mute = true;
+            heartbeatFastSfx.mute = true;
+        }
+    }
+}
