f6af00a19184cb7f96e2ebcdd2e5121348dd1b3b
UfukGlr <ugueler54@gmail.com>
2023-07-15 21:22:14 +0200

    - various changes

---
+++ Assets/LowHealthFx.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class LowHealthFx : MonoBehaviour
+{
+    private Volume globalVolume;
+    private Vignette vignette;
+    [SerializeField] private AnimationCurve vignetteIntensityCurve;
+    private GameObject currentPlayer; // Referenz auf den aktuellen Player
+    public AudioSource fastHeartBeatSfx;
+    
+    private void Start()
+    {
+        currentPlayer = GameObject.FindGameObjectWithTag("Player");
+        globalVolume = GameObject.FindGameObjectWithTag("GlobalVolume").GetComponent<Volume>();
+        globalVolume.profile.TryGet(out vignette);
+    }
+
+    private void Update()
+    {
+        LowHealthEffect();
+        HeartBeat();
+    }
+
+    private void LowHealthEffect()
+    {
+        if (vignette != null)
+        {
+            Player player = currentPlayer.GetComponent<Player>();
+            float currentHealth = player.GetCurrentHealth();
+            float healthPercentage = currentHealth / player.GetMaxHealth(); // Prozentsatz der verbleibenden Gesundheit (0-1)
+
+            if (currentHealth <= 50)
+            { 
+                vignette.color.Override(new Color(0.2f, 0f, 0f));
+                float intensity = vignetteIntensityCurve.Evaluate(1f - healthPercentage); // Invertieren des Prozentsatzes
+                vignette.intensity.Override(intensity);
+            }
+            else
+            {
+                vignette.color.Override(Color.black);
+                vignette.intensity.Override(0.25f);
+            }
+        }
+    }
+
+    private void HeartBeat()
+    {
+        Player player = currentPlayer.GetComponent<Player>();
+        float currentHealth = player.GetCurrentHealth();
+        if (currentHealth <= 50)
+        {
+            fastHeartBeatSfx.mute = false;
+        }
+        else
+        {
+            fastHeartBeatSfx.mute = true;
+        }
+    }
+}
