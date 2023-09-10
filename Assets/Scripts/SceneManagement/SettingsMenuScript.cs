a025d1c3c857c5fcc3e9e40da6684a74e86ef811
Mortan1337 <martinholstein@live.de>
2023-09-10 14:23:33 +0200

    Code Doku Added

--- Assets/Scripts/SceneManagement/SettingsMenuScript.cs
+++ Assets/Scripts/SceneManagement/SettingsMenuScript.cs
@@ -1,3 +1,5 @@
+/* created by: SWT-P_SS_23_akin75 */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +16,11 @@ public static bool HealthBarOn = true;
 [SerializeField] Toggle toogle_Mute;
 [SerializeField] Toggle toogle_HealthBar;
 [SerializeField] Toggle something;
-    // Update is called once per frame
+
+    /// <summary>
+    /// Here the player can adjust settings used in the game.
+    /// <summary>
+
     void Start()
     {
         if(!PlayerPrefs.HasKey("musicVolume"))
@@ -28,24 +34,32 @@ public static bool HealthBarOn = true;
             Load();
         }
     }
-    
+
+    /// <summary>
+    /// The player can adjust the global audio. The settings get saved in the player prefs.
+    /// <summary>
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
         Save();
         volume.SetText(Mathf.Round((volumeSlider.value * 100)).ToString());
     }
-
+    /// <summary>
+    /// Loads the audio settings from player prefs into the audio settings.
+    /// <summary>
     private void Load(){
         volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
 
     }
-
+    /// <summary>
+    /// Saves the audio settings in the player prefs.
+    /// <summary>
     private void Save(){
-       
         PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
-
+    /// <summary>
+    /// Returns the player to the main menu and applies the settings to the game.
+    /// <summary>
      public void ReturnToMainMenu(){
        if (toogle_Mute.isOn)
 			{AudioListener.volume = 0f;}
@@ -62,6 +76,9 @@ public static bool HealthBarOn = true;
     SceneManager.LoadScene("MainMenu");//Ruft die Szene in der Klammer auf
      }
 
+    /// <summary>
+    /// Enables and disables the healthbars of enemies and the player.
+    /// <summary>
     public bool HealthBarIsEnabled(){
         return HealthBarOn;
     } 
