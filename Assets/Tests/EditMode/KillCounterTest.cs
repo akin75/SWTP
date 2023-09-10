a9a8f5bb9b528fa835452420e9de6d81eac20172
Nel <ratchapanya1998@gmail.com>
2023-09-10 21:33:58 +0200

    change test

--- Assets/Tests/EditMode/KillCounterTest.cs
+++ Assets/Tests/EditMode/KillCounterTest.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
-
+using UnityEditor;
+using UnityEngine;
+using TMPro;
 [TestFixture]
 public class KillCounterTests
 {
@@ -7,8 +9,14 @@ public class KillCounterTests
     public void InitialKillCount_ShouldBeZero()
     {
         // Arrange
-        KillCounter killCounter = new KillCounter();
+        var killCounterObject = new GameObject();
+        var killCounter = killCounterObject.AddComponent<KillCounter>();
 
+        var textPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/SceneManager/Kills.prefab");
+
+        var sObject = new SerializedObject(killCounter);
+        sObject.FindProperty("killCounterText").objectReferenceValue = textPrefab;
+        sObject.ApplyModifiedProperties();
         // Assert
         Assert.AreEqual(0, killCounter.GetKills());
     }
@@ -17,9 +25,14 @@ public class KillCounterTests
     public void IncreaseKillCount_ShouldIncrementByOne()
     {
         // Arrange
-        KillCounter killCounter = new KillCounter();
+        var killCounterObject = new GameObject();
+        var killCounter = killCounterObject.AddComponent<KillCounter>();
 
-        // Act
+        var textPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/SceneManager/Kills.prefab");
+
+        var sObject = new SerializedObject(killCounter);
+        sObject.FindProperty("killCounterText").objectReferenceValue = textPrefab;
+        sObject.ApplyModifiedProperties();
         killCounter.IncreaseKillCount();
 
         // Assert
